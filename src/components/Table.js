a7aa134720fb5dfaa5657ac3709f60023012fdc6
Kushagra <tibrewalkushagra@gmail.com>
2023-07-12 17:30:04 +0530

    Rounded Off the values

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,77 +1,83 @@
 import React, { useEffect, useState } from "react";
 
-const Table = ({data,  months}) => {
+const Table = ({ data, months }) => {
 
-  const [productData,setProductData] = useState(null)
+  const [productData, setProductData] = useState(null)
   // console.log('data',data)
-  useEffect(()=>{
+  useEffect(() => {
     setProductData(data)
-  },[data])
+  }, [data])
 
   // console.log('months',months)
   // const months = ['may']
   // const months = ['april','may','june','july','august','september','october','november','december','january','february','march']
   const headerRow = [
-    'Particulars',...months,
+    'Particulars', ...months,
   ]
 
   const parameters = [
     {
-      displayValue:'Count (Nos.)',
-      dataValue:'count'
+      displayValue: 'Count (Nos.)',
+      dataValue: 'count'
     },
     {
-      displayValue:'Volume (₹)',
-      dataValue:'volume'
+      displayValue: 'Volume (₹)',
+      dataValue: 'volume'
     },
     {
-      displayValue:'Pay-In (₹)',
-      dataValue:'payIn'
+      displayValue: 'Pay-In (₹)',
+      dataValue: 'payIn'
     },
     {
-      displayValue:'Pay-Out (₹)',
-      dataValue:'payOut'
+      displayValue: 'Pay-Out (₹)',
+      dataValue: 'payOut'
     },
     {
-      displayValue:'Retention (₹)',
-      dataValue:'retention',
+      displayValue: 'Retention (₹)',
+      dataValue: 'retention',
     }
   ]
 
   return productData && <div className="table-container">
-  <table style={{ width: '100%' }}>
-    <thead>
-      <tr>
+    <table style={{ width: '100%' }}>
+      <thead>
+        <tr>
+          {
+            headerRow.map((hr, index) => {
+              return <th key={index}>
+                {hr.displayValue?.charAt(0).toUpperCase() + hr.displayValue?.slice(1) || hr}
+              </th>
+            })
+          }
+        </tr>
+      </thead>
+      <tbody>
         {
-          headerRow.map((hr,index)=>{
-            return <th key={index}>
-              {hr.displayValue?.charAt(0).toUpperCase() + hr.displayValue?.slice(1) || hr}
-            </th>
+          parameters.map((p, index) => {
+            return (
+              <tr key={index}>
+                <td>{p.displayValue}</td>
+                {
+                  months.map((m, index2) => {
+                    var roundedString;
+                    if (productData[m.value]) {
+                      const mynumber = parseFloat(`${productData[m.value][p.dataValue]}`)
+                      const roundedNumber = Math.round(mynumber);
+                      roundedString = roundedNumber.toString();
+                      // console.log('m',`${}`);
+                    }
+                    return (
+                      productData[m.value] ? <td key={index2}>{roundedString}</td> : <td key={index2}></td>
+                    );
+                  })
+                }
+              </tr>
+            )
           })
         }
-      </tr>
-    </thead>
-    <tbody>
-      {
-        parameters.map((p,index)=>{
-          return (
-            <tr key={index}>
-              <td>{p.displayValue}</td>
-              {
-                months.map((m,index2)=>{
-                  // console.log('m',m,m.value)
-                  return (
-                    productData[m.value] ? <td key={index2}>{productData[m.value][p.dataValue]}</td> : <td key={index2}></td>
-                  );
-                })
-              }
-            </tr>
-          )
-        })
-      }
-    </tbody>
-  </table>
-</div>;
+      </tbody>
+    </table>
+  </div>;
 };
 
 export default Table;
